52c34ee726ebf57e81b20f9b83432efc013dfe15
= <918780@dpsk12.net>
2023-08-16 10:56:17 -0600

    remove a bunch of spaces

--- rick_Roll.ps1
+++ rick_Roll.ps1
@@ -1,12 +1,6 @@
 ﻿write-output 'your computer is now mine '
-
-while($val -ne 20 ) { $val++ ; 
-
+while($val -ne 0 ) { $val++ ; 
 	start chrome https://www.youtube.com/watch?v=dQw4w9WgXcQ
-	
 }
-
 write-output ( 'you have been rick-rolled ' + $val + ' times' )
-
 Read-Host -Prompt "enter your social security number please "
-
